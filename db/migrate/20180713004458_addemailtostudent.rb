b909b3265e5c36afaa4c7a58dc8ddad70457b82c
patriciavera <patrverab@gmail.com>
2018-07-12 21:16:13 -0400

    cuarto commit

---
+++ db/migrate/20180713004458_addemailtostudent.rb
@@ -0,0 +1,4 @@
+class Addemailtostudent < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180712140808) do
+ActiveRecord::Schema.define(version: 20180713010903) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
